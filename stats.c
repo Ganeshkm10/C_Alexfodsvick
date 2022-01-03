5fa6f047628336e6853752a95af60cae59dfff5e
Ganesh KM <gaka6506@colorado.edu>
2022-01-03 15:07:42 -0700

    Coursera assignment 1

--- stats.c
+++ stats.c
@@ -9,13 +9,11 @@
  *
  *****************************************************************************/
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * @file <stats.c> 
+ * @brief <For Coursera assignment 1>>
  *
- * <Add Extended Description Here>
- *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * @author <Ganesh KM>
+ * @date <03/01/2022>
  *
  */
 
@@ -27,6 +25,7 @@
 /* Size of the Data Set */
 #define SIZE (40)
 
+/* The main entry point of program */
 void main() {
 
   unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
@@ -35,9 +34,96 @@ void main() {
                               201,   6,  12,  60,   8,   2,   5,  67,
                                 7,  87, 250, 230,  99,   3, 100,  90};
 
-  /* Other Variable Declarations Go Here */
-  /* Statistics and Printing Functions Go Here */
+ print_array(test);
+ print_statistics(test);
+ sort_array(test);
+}
+
+/* Given an array of data, returns the mean */
+
+float find_mean(unsigned char *ptr)
+{
+   int sum=0;
+   for(int i=0;i<SIZE;i++)
+   {
+   
+   sum = sum+ptr[i];
+   
+   }
+return (float)sum/SIZE;
+}
+
+/* A function that prints the statistics of an array including minimum, maximum, mean, and median. */
+
+int print_statistics(unsigned char *ptr)
+{
+printf(" \n---- Array Statistics ---- \n");
+printf(" Mean of the given array is %f \n", find_mean(ptr));
+printf(" Median of the given array is %f \n", find_median(ptr));
+printf(" Maximum element of the given array is %d \n", find_maximum(ptr));
+printf(" Minimum element of the given array is %d \n", find_minimum(ptr));
+
 
 }
 
-/* Add other Implementation File Code Here */
+/* Descending sort function */
+void sort(unsigned char *ptr)
+{
+unsigned char temp;
+for(int i=0;i<SIZE;i++)
+{
+ for (int j=i+1;j<SIZE;j++)
+ {
+   if(ptr[j]>ptr[i])
+   {
+   	temp = ptr[j];
+   	ptr[j]=ptr[i];
+   	ptr[i]=temp;
+   }
+  }
+}
+}
+
+/* Function to print the sorted array */
+void sort_array(unsigned char *ptr) 
+{ 
+ sort(ptr);
+printf("\n The Sorted array is \n");
+for(int i=0;i<SIZE;i++)
+{
+printf("%d ",ptr[i]);
+}
+printf("\n");
+}
+
+/* Function to print the given array */
+void print_array(unsigned char *ptr) 
+{
+printf("The unsorted array is \n");
+ for(int i=0;i<SIZE;i++)
+ {
+ printf("%d ",ptr[i]);
+ }
+}
+/* Function to return the maximum element in an array */
+int find_maximum(unsigned char *ptr)
+{
+  sort(ptr);
+  return ptr[0];
+}
+/* Function to return the mininum element in an array */
+int find_minimum(unsigned char *ptr) 
+{
+  sort(ptr);
+  return ptr[SIZE-1];
+}
+
+/* Function to return the median in the given array */
+float find_median(unsigned char *ptr)
+{
+  sort(ptr);
+  if(SIZE % 2 !=0)
+  return (float)ptr[SIZE/2];
+  else
+  return (float)(ptr[(SIZE-1)/2] + ptr[SIZE/2])/2.0;
+}
--- stats.h
+++ stats.h
@@ -9,32 +9,95 @@
  *
  *****************************************************************************/
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * @file <stats.h> 
+ * @brief <Header file for the assignment 1 >
  *
- * <Add Extended Description Here>
- *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * @author Ganesh KM
+ * @date 03/01/2022
  *
  */
 #ifndef __STATS_H__
 #define __STATS_H__
 
-/* Add Your Declarations and Function Comments here */ 
 
 /**
- * @brief <Add Brief Description of Function Here>
+ * @brief Given an array of data, returns the mean
  *
- * <Add Extended Description Here>
+ * @param <ptr> <Test array>
+
  *
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
+ * @return <mean
+ */
+float find_mean(unsigned char *ptr);
+
+/**
+ * @brief < function that prints the statistics of an array including minimum, maximum, mean, and median
  *
- * @return <Add Return Informaiton here>
+ * @param <ptr> <Test array>
+
+ *
+ * @return None
  */
 
+int print_statistics(unsigned char *ptr);
+
+/**
+ * @brief Function to print the sorted array
+ *
+ * @param <ptr> <Test array>
+
+ *
+ * @return NONE
+ */
+void sort_array(unsigned char *ptr) ;
+
+/**
+ * @brief Descending Sort
+ *
+ * @param <ptr> <Test array>
+ *
+ * @return none
+ */
+void sort(unsigned char *ptr);
+
+/**
+ * @brief Function to print the given array 
+ *
+ * @param <ptr> <Test array>
+
+ *
+ * @return none
+ */
+void print_array(unsigned char *ptr) ;
+
+/**
+ * @brief Function to return the maximum element in an array 
+ *
+ * @param <ptr> <Test array>
+
+ *
+ * @return maximum
+ */
+int find_maximum(unsigned char *ptr);
+
+/**
+ * @brief Function to return the minimum element in an array 
+ *
+ * @param <ptr> <Test array>
+
+ *
+ * @return minimum
+ */
+int find_minimum(unsigned char *ptr) ;
+
+/**
+ * @brief Function to return the median in the given array 
+ *
+ * @param <ptr> <Test array>
+
+ *
+ * @return median
+ */
+float find_median(unsigned char *ptr);
 
 #endif /* __STATS_H__ */
